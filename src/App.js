c40ddf6389a406395bebdad0a645e806a2f163f1
matthajec <66030008+matthajec@users.noreply.github.com>
2021-02-02 06:53:16 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import reactLogo from './react.gif';
+
+const App = () => (
+  <div>
+    <img src={reactLogo} />
+    <h1>Hello React</h1>
+  </div>
+);
+
+export default App;
